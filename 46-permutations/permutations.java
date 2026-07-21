642f15e0c5f9a16faf6a97b235a5397232384583
MunagalaDhanalaxmi <munagaladhanalaxmimrecw7@gmail.com>
2026-07-21 11:06:44 +0530

    Time: 1 ms (87.05%) | Memory: 45.5 MB (46.45%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -5,11 +5,13 @@ class Solution {
         backtrack(nums, new ArrayList<>(), visited, result);
         return result;
     }
+
     private void backtrack(int[] nums, List<Integer> current, boolean[] visited, List<List<Integer>> result) {
         if (current.size() == nums.length) {
             result.add(new ArrayList<>(current));
             return;
         }
+        
         for (int i = 0; i < nums.length; i++) {
             if (visited[i]) continue;
             visited[i] = true;
